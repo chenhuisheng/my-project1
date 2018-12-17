a7b31a85562f365e0be7f7a04381e2fb6a6e4a9f
abc <599874882@qq.com>
2018-12-17 09:41:34 +0800

    changes log

--- src/components/project_list.vue
+++ src/components/project_list.vue
@@ -1,12 +1,13 @@
 <template>
-    <div id="app">
+    <!-- <div id="app" @click="getAllList($event)"> -->
+    <div>
         <table class="table table-bordered table-hover table-stripet">
             <tr>
-                <td>Id</td>
+                <td>name</td>
                 <td>title</td>
             </tr>
-            <tr v-for="item in times" :key="item.id">
-                <td>{{item.id}}</td>
+            <tr v-for="item in times" :key="item.id" >
+                <td>{{item.name}}</td>
                 <td>
                 <router-link :to="{ name: 'path', params: { item: item }}">{{item.title}}</router-link>
                 </td>
@@ -16,12 +17,15 @@
 </template>
 
 <script>
-    import axios from "axios"
+    import axios from "axios" 
     export default {
         name: 'project_list',
         data () {
             return {
-                times:'123'
+                times:[],
+                data:[],
+                ment:[],
+                item:''
             }
         },
         created(){
@@ -30,10 +34,28 @@
         methods:{
             getAllList(){
                 axios.get("https://jsonplaceholder.typicode.com/posts").then(result=>{
-                    this.times=result.data
-                })
+                    this.data=result.data,
+                    this.get_user()});
+            },
+            get_user(){
+                for (var i=0; i<this.data.length; i++){
+                    var id = this.data[i].userId
+                    this.get_ment(id, i)
+                    }
+            },
+            get_ment(id, i){
+                axios.get("https://jsonplaceholder.typicode.com/users", {params:{id}}).then(result=>{
+                        this.ment=result.data;
+                        var name =result.data[0].name;
+                        this.times.push({
+                            id: this.data[i].id,
+                            title: this.data[i].title,
+                            body:this.data[i].title,
+                            name: name
+                            })
+                        })
             }
-        },
+        }
         }
 </script>
 
